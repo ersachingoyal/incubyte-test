c43312568a83761376fef7f2c83d76cff7184875
Sachin goyal <goyalsachin176@gmail.com>
2024-05-17 13:41:54 +0530

    handled negative case

--- addNumbers.js
+++ addNumbers.js
@@ -5,13 +5,19 @@ function add(numbers) {
     
     let delimiter = ',';
     if (numbers.startsWith('//')) {
-        const parts = numbers.split('\n', 2);
+        const parts = numbers.split('\n');
         delimiter = parts[0].substring(2);
         numbers = parts[1];
     }
     
     numbers = numbers.replace(/\n/g, delimiter);
     const numArray = numbers.split(delimiter);
+    
+    const negatives = numArray.filter(num => parseInt(num, 10) < 0);
+    if (negatives.length > 0) {
+        throw new Error(`negative numbers not allowed ${negatives.join(',')}`);
+    }
+    
     const sum = numArray.reduce((acc, num) => acc + parseInt(num, 10), 0);
     return sum;
 }

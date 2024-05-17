7c156f55a01a899e39584bc5bb12892a4d5baa67
Sachin goyal <goyalsachin176@gmail.com>
2024-05-17 13:41:10 +0530

    handled starting with different character

--- addNumbers.js
+++ addNumbers.js
@@ -3,8 +3,15 @@ function add(numbers) {
         return 0;
     }
     
-    numbers = numbers.replace(/\n/g, ',');
-    const numArray = numbers.split(',');
+    let delimiter = ',';
+    if (numbers.startsWith('//')) {
+        const parts = numbers.split('\n', 2);
+        delimiter = parts[0].substring(2);
+        numbers = parts[1];
+    }
+    
+    numbers = numbers.replace(/\n/g, delimiter);
+    const numArray = numbers.split(delimiter);
     const sum = numArray.reduce((acc, num) => acc + parseInt(num, 10), 0);
     return sum;
 }
